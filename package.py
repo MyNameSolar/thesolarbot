5589c9dfb37335e5cfa8c15687a7f3efc0eb0011
MyNameSolar <43095153+MyNameSolar@users.noreply.github.com>
2018-09-09 09:59:02 +0100

    Create package.py

---
+++ package.py
@@ -0,0 +1,13 @@
+{
+  "name": "SolarBot",
+  "description": "SolarBot",
+  "version": "0.0.0",
+  "main": "Bot.py",
+  "scripts": {
+    "start": "node Bot.py"
+  },
+  "dependencies": {
+    "discord.py": "11.1.0",
+    "request": "2.81.0"
+  }
+}
